7acf3592f4d8233a452afcf796bcd704056ae1ba
bar-amit <bar.amit@outlook.com>
2022-03-10 09:48:24 +0200

    build: favicon, title and manifest

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,7 +9,7 @@ function Header() {
       <h1 className="header__title">
         Hello, My name is <span className="header__highlight">Bar Amit</span>.
       </h1>
-      <p className="header__description"> I am a full-stack web developer.</p>
+      <p className="header__description"> I am a front-end web developer.</p>
       <Link className="header__button" to="about" smooth={true}>
         About me
       </Link>
